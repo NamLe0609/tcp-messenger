1f7e256df92aaa5e80e5eb3a94a97d80a6442da6
NamLe0609 <lehoangnamtep@gmail.com>
2023-12-18 09:31:42 +0700

    Add select code for server

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import sys
 
 import socket
-import threading
 import logging
+import threading
 import select
 
 #logging.basicConfig(filename='server.log', filemode='w', format='%(levelname)s:%(message)s', level=logging.DEBUG)
@@ -15,11 +15,18 @@ def main():
     PORT = 1234
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(False)
     server.bind((HOST, PORT))
-    server.listen()
+    server.listen(5)
     
     clients = dict()
-    receive(server=server, clients=clients)
+    inputs = [server]
+    outputs = []
+    
+    receive(server=server, 
+            clients=clients,
+            inputs=inputs,
+            outputs=outputs)
 
 def broadcast(message, clients):
     for client in clients:
@@ -37,22 +44,25 @@ def handle(client, clients):
             broadcast(f'{nickname} just left!'.encode('ascii'), clients)
             break
             
-
-def receive(server, clients):
+def receive(server, clients, inputs, outputs):
     while True:
-        client, address = server.accept()
-        print(f'Connected with {str(address)}')
-        
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        clients[client] = nickname
-        
-        print(f'Client nickname is {nickname}')
-        broadcast(f'{nickname} joined!'.encode('ascii'), clients)
-        
-        thread = threading.Thread(target=handle, args=(client, clients,))
-        thread.start()
-        
+        r,w,e = select.select(inputs, outputs, inputs, 0.5)
+        for s in r:
+            if s == server:
+                client, address = s.accept()
+                inputs.append(client)
+                print(f'Connected with {str(address)}')
+                
+                client.send('NICK'.encode('ascii'))
+                nickname = client.recv(1024).decode('ascii')
+                clients[client] = nickname
+                
+                print(f'Client nickname is {nickname}')
+                broadcast(f'{nickname} joined!'.encode('ascii'), clients)
+                
+                thread = threading.Thread(target=handle, args=(client, clients,))
+                thread.start()
+                
 if __name__ == '__main__':
     main()
     
