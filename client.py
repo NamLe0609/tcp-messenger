0a40432d846d5baec421aee5228a4641aef85ea2
NamLe0609 <lehoangnamtep@gmail.com>
2023-12-19 17:16:57 +0700

    Make code pep8 conforming

--- client.py
+++ client.py
@@ -1,33 +1,31 @@
+"""Module providing functionality for networks programming"""
 import socket
 import threading
 import sys
 
-# Choose encoding method to use for everything
+HEADERSIZE = 10
 ENCODING = 'utf-8'
 
 class Client:
-    
+    """Class representing a client"""
     def __init__(self, username='John_Pork', host='127.0.0.1', port='1234'):
         # Setup client socket
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
         self.username = username
-        
         # Define two seperate thread handling reads and writes from and to the server
-        self.receiveThread = threading.Thread(target=self.receive, args=())
-        self.writeThread = threading.Thread(target=self.write, args=())
-        
+        self.receive_thread = threading.Thread(target=self.receive, args=())
+        self.write_thread = threading.Thread(target=self.write, args=())
         # State to check if client should be running
         self.running = True
 
     def receive(self):
+        """Function to receive messages from the server"""
         while self.running:
             message = self.client.recv(1024).decode(ENCODING)
-            
             # If the other thread is not alive then quit
-            if not self.writeThread.is_alive():
+            if not self.write_thread.is_alive():
                 sys.exit(0)
-                
             if message:
                 print(message)
             else:
@@ -35,22 +33,20 @@ class Client:
                 self.running = False
                 self.client.close()
                 sys.exit(0)
-    
     def write(self):
+        """Function to send messages to the server"""
         while self.running:
             body = input()
-            
             # If the other thread is not alive then quit
-            if not self.receiveThread.is_alive():
+            if not self.receive_thread.is_alive():
                 sys.exit(0)
-                
             # Commands start with a '/'
             if body and body[0] == '/':
-                self.runCommand(body[1:])
+                self.run_command(body[1:])
             else:
                 self.client.send(body.encode(ENCODING))
-            
-    def runCommand(self, command):
+    def run_command(self, command):
+        """Function to run commands when a forward slash given"""
         match command:
             case 'wisper':
                 pass
@@ -61,14 +57,10 @@ class Client:
                 self.running = False
                 self.client.send('/leave'.encode(ENCODING))
                 sys.exit(0)
-                
-            case default:
-                print('Invalid command')
-        
     def run(self):
-        self.receiveThread.start()
-        self.writeThread.start()
-        
+        """Function to run the client object"""
+        self.receive_thread.start()
+        self.write_thread.start()
         # Send username to server
         self.client.send(self.username.encode(ENCODING))
 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
+"""Module providing functionality for networks programming"""
 import sys
-
 import socket
 import threading
 import logging
@@ -7,101 +7,89 @@ import logging
 #logging.basicConfig(filename='server.log', filemode='w', format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logging.basicConfig(format='%(levelname)s - %(asctime)s: %(message)s', level=logging.DEBUG)
 
+HEADERSIZE = 10
+ENCODING = 'utf-8'
+
 class Server:
+    """Class representing a server"""
     def __init__(self, host='127.0.0.1', port=1234):
         # Choose encoding method to use for everything
-        self.ENCODING = 'utf-8'
-        
         # Setup server socket
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
         self.server.listen()
-        
         # Setup storage for client info in the form (address, username)
         self.clients = dict()
-        self.takenNames = set()
+        self.taken_names = set()
 
     def broadcast(self, message, mode=0, broadcaster=None, broadcastee=None):
+        """Function to send messages"""
+        # Append a fixed size header
+        message = f'{len(message):<{HEADERSIZE}}' + message
         match mode:
             # Broadcast to all (Server mode)
             case 1:
-                message = f'[SERVER]: ' + message
+                message = '[SERVER]: ' + message
                 for client in self.clients:
-                    client.send(message.encode(self.ENCODING))
-            
+                    client.send(message.encode(ENCODING))
             # Broadcast to all but broadcaster
             case 2:
                 message = f'[{self.clients[broadcaster][1]}]: {message}'
                 for client in self.clients:
                     if client != broadcaster:
-                        client.send(message.encode(self.ENCODING))
-            
+                        client.send(message.encode(ENCODING))
             # Broadcast to individual
             case 3:
-                broadcastee.send(message.encode(self.ENCODING))
-                
+                broadcastee.send(message.encode(ENCODING))
     def handle(self, client):
+        """Function to handle messages received from clients"""
         while True:
-            try:
-                message = client.recv(1024).decode(self.ENCODING)
-                if not message:
-                    self.killConnection(client)
-                elif message[0] == '/':
-                    self.executeCommand(message[1:], client=client)
-                else:
-                    self.broadcast(message, 2, broadcaster=client)
-            except:
-                break
-    
-    def killConnection(self, client):
+            message = client.recv(1024).decode(ENCODING)
+            if not message:
+                self.kill_connection(client)
+            elif message[0] == '/':
+                self.ru_command(message[1:], client=client)
+            else:
+                self.broadcast(message, 2, broadcaster=client)
+    def kill_connection(self, client):
+        """Function to kill connection to unresponsive clients"""
         address, username = self.clients[client]
         client.close()
         self.clients.pop(client)
-        self.takenNames.remove(username)
+        self.taken_names.remove(username)
         self.broadcast(f'{username} just left. Goodbye!', 1)
         print(f'Disonnected with {str(address)}. Remove client named {username}')
-    
-    def executeCommand(self, command, client=None):
+    def ru_command(self, command, client=None):
+        """Function to run commands when a forward slash given"""
         match command:
             case 'wisper':
                 pass
             case 'help':
                 pass
             case 'leave':
-                self.killConnection(client)
-            case default:
-                print('Invalid command')
-        
-    
+                self.kill_connection(client)
     def run(self):
+        """Function to run The server"""
         while True:
             client, address = self.server.accept()
-            username = client.recv(1024).decode(self.ENCODING)
-            
+            username = client.recv(1024).decode(ENCODING)
             # Disallow duplicate username in chat
-            if username in self.takenNames:
-                self.broadcast(f'Username taken', 3, broadcastee=client)
+            if username in self.taken_names:
+                self.broadcast('Username taken', 3, broadcastee=client)
                 client.close()
                 continue
-            else:
-                self.broadcast(f'Instructions: [/leave] to exit, [/whisper] to directly message someone, [/help] to show instructions', 3, broadcastee=client)
-            
             # Store clients' address and username with the socket as key
             self.clients[client] = (address, username)
-            self.takenNames.add(username)
-            
+            self.taken_names.add(username)
             # Broadcast after as it loops over clients dictionary
             print(f'Connected with {str(address)}. Add client named {username}')
             self.broadcast(f'{username} just joined. Welcome!', 1)
-            
             thread = threading.Thread(target=self.handle, args=(client,))
             thread.start()
-        
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('Need 1 arguments')
         sys.exit()
-    
     server = Server(port=int(sys.argv[1]))
     server.run()
     
