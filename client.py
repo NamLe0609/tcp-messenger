f150fadedf2265d4682564f17852123d1da713ae
NamLe0609 <lehoangnamtep@gmail.com>
2023-12-19 12:12:14 +0700

    Add comments

--- client.py
+++ client.py
@@ -5,66 +5,79 @@ import sys
 class Client:
     
     def __init__(self, username='John_Pork', host='127.0.0.1', port='1234'):
+        # Choose encoding method to use for everything
+        self.ENCODING = 'utf-8'
+        
+        # Setup client socket
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
         self.username = username
-        self.client.send(self.username.encode('ascii'))
         
-        self.writeThread = threading.Thread(target=self.write, args=())
+        # Define two seperate thread handling reads and writes from and to the server
         self.receiveThread = threading.Thread(target=self.receive, args=())
+        self.writeThread = threading.Thread(target=self.write, args=())
+        
+        # State to check if client should be running
+        self.running = True
 
     def receive(self):
-        while True:
-            try:
-                message = self.client.recv(1024).decode('ascii')
-                if not self.writeThread.is_alive():
-                    sys.exit(0)
-                if message:
-                    print(message)
-                else:
-                    print('Something went wrong. Disconnecting.')
-                    self.client.close()
-                    sys.exit(0)
-            except:
-                break
+        while self.running:
+            message = self.client.recv(1024).decode(self.ENCODING)
+            
+            # If the other thread is not alive then quit
+            if not self.writeThread.is_alive():
+                sys.exit(0)
+                
+            if message:
+                print(message)
+            else:
+                print('Something went wrong. Disconnecting...')
+                self.running = False
+                self.client.close()
+                sys.exit(0)
 
     def write(self):
-        while True:
+        while self.running:
             body = input('')
             message = ''
             
+            # If the other thread is not alive then quit
             if not self.receiveThread.is_alive():
                 sys.exit(0)
                 
-            if body[0] == '/':
-                self.runCommand(body)
+            # Commands start with a '/'
+            if body and body[0] == '/':
+                self.runCommand(body[1:])
             else:
                 message = f'{self.username}: {body}'
-                self.client.send(message.encode('ascii'))
+                self.client.send(message.encode(self.ENCODING))
             
     def runCommand(self, command):
-        match command[1:]:
+        match command:
             case 'wisper':
                 pass
             case 'help':
                 pass
             case 'leave':
                 print('Quitting...')
-                self.client.send('/leave'.encode('ascii'))
+                self.running = False
+                self.client.send('/leave'.encode(self.ENCODING))
                 sys.exit(0)
                 
             case default:
                 print('Invalid command')
         
     def run(self):
-        self.receiveThread.start()
         self.writeThread.start()
+        self.receiveThread.start()
+        
+        # Send username to server
+        self.client.send(self.username.encode(self.ENCODING))
 
 if __name__ == '__main__':
     if len(sys.argv) <= 3:
         print('Need 3 arguments')
         sys.exit()
-    
     client = Client(username=sys.argv[1], host=sys.argv[2], port=int(sys.argv[3]))
     client.run()
     
--- server.py
+++ server.py
@@ -9,25 +9,32 @@ logging.basicConfig(format='%(levelname)s - %(asctime)s: %(message)s', level=log
 
 class Server:
     def __init__(self, host='127.0.0.1', port=1234):
+        # Choose encoding method to use for everything
+        self.ENCODING = 'utf-8'
+        
+        # Setup server socket
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
         self.server.listen()
         
+        # Setup storage for client info
         self.clients = dict()
         self.takenNames = set()
 
     def broadcast(self, message, mode=0, broadcaster=None, broadcastee=None):
-        message = message.encode('ascii')
+        message = message.encode(self.ENCODING)
         match mode:
             # Broadcast to all
-            case 1:    
+            case 1:
                 for client in self.clients:
                     client.send(message)
+            
             # Broadcast to all but broadcaster
             case 2:
                 for client in self.clients:
                     if client != broadcaster:
                         client.send(message)
+            
             # Broadcast to individual
             case 3:
                 broadcastee.send(message)
@@ -35,13 +42,13 @@ class Server:
     def handle(self, client):
         while True:
             try:
-                message = client.recv(1024).decode('ascii')
+                message = client.recv(1024).decode(self.ENCODING)
                 if not message:
                     self.killConnection(client)
                 elif message[0] == '/':
                     self.executeCommand(message[1:], client=client)
                 else:
-                    self.broadcast(message, 2, client)
+                    self.broadcast(message, 2, broadcaster=client)
             except:
                 break
     
@@ -50,7 +57,7 @@ class Server:
         client.close()
         self.clients.pop(client)
         self.takenNames.remove(username)
-        self.broadcast(f'{username} just left!', 1)
+        self.broadcast(f'{username} just left. Goodbye!', 1)
         print(f'Disonnected with {str(address)}. Remove client named {username}')
     
     def executeCommand(self, command, client=None):
@@ -68,17 +75,21 @@ class Server:
     def run(self):
         while True:
             client, address = self.server.accept()
-            username = client.recv(1024).decode('ascii')
+            username = client.recv(1024).decode(self.ENCODING)
+            
+            # Disallow duplicate username in chat
             if username in self.takenNames:
-                self.broadcast(f'Username taken', 3, client)
+                self.broadcast(f'Username taken', 3, broadcastee=client)
                 client.close()
                 continue
-            else:
-                print(f'Connected with {str(address)}. Add client named {username}')
-                self.broadcast(f'{username} just joined. Welcome!', 1)
             
+            # Store clients' address and username with the socket as key
             self.clients[client] = (address, username)
             self.takenNames.add(username)
+            
+            # Broadcast after as it loops over clients dictionary
+            print(f'Connected with {str(address)}. Add client named {username}')
+            self.broadcast(f'{username} just joined. Welcome!', 1)
             
             thread = threading.Thread(target=self.handle, args=(client,))
             thread.start()
